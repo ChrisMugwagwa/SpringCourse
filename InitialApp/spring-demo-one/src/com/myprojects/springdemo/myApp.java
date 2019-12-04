e9cf8bbbe584dba9c05d07b9496ef855088d854e
Christian Mugwagwa <mugwagwachristian@gmail.com>
2019-12-04 18:52:58 +0000

    added example of bean scoping using prototype as opposed to singleton

--- InitialApp/spring-demo-one/src/com/myprojects/springdemo/myApp.java
+++ InitialApp/spring-demo-one/src/com/myprojects/springdemo/myApp.java
@@ -12,11 +12,20 @@ public class myApp {
 		
 		//Retrieve the bean from the spring container
 		Coach theCoach = context.getBean("myCoach", BoxingCoach.class);
+		BoxingCoach secondCoach = context.getBean("myCoach", BoxingCoach.class);
 		
+		secondCoach.setAge(52);
+		secondCoach.setEmail("second@coach.com");
 		//call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getAdvice());
 		System.out.println(theCoach.getInformation());
+		
+		
+		System.out.println("This second coach is made using the prototype scoping so it is a different instance");
+		System.out.println(secondCoach.getDailyWorkout());
+		System.out.println(secondCoach.getAdvice());
+		System.out.println(secondCoach.getInformation());
 		//close the context
 		context.close();
 	}
